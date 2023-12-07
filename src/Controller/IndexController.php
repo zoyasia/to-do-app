089142ea378025d915af8b4f98f41b83e84a6483
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-12-07 14:42:25 +0100

    commit before merge

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
